30dd80aa533275063a9a3718ae629bbe66e96f1f
akshanshgusain <akshanshgusain@gmail.com>
2022-03-01 18:11:36 +0530

    feat(app.js): First GraphQL API.

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+    type RootMutation{
+        createEvent(name: String): String
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }`),
+    rootValue: {
+      events: () => {
+        return ["Event1", "Event2", "Event3"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.get("/", (req, res, next) => {
   res.send("Hey There!");
