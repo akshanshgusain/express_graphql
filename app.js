ff19534b28cf1bf5d2422ab9665e2ed1a376b47a
akshanshgusain <akshanshgusain@gmail.com>
2022-03-04 16:50:52 +0530

    feat: added getevents and create events

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const isAuth = require("./middleware/is-auth");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -33,8 +44,8 @@ const dbConnect = async () => {
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustergraphql.okwmj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
     );
 
-    app.listen(3000, () => {
-      console.log(`listening to port 3000!`);
+    app.listen(8000, () => {
+      console.log(`listening to port 8000!`);
     });
   } catch (err) {
     console.log("Failed to start the server! Could not connect to MongoDB");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,78 @@ import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import MainNavigation from "./components/navigation/MainNavigation";
 import "./App.css";
+import AuthContext from "./context/auth-context";
+import { Component } from "react";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main className="main-content">
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate replace to="/auth"></Navigate>}
-            exact
-          />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/bookings" element={<BookingsPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null,
+  };
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            token: this.state.token,
+            userId: this.state.userId,
+            login: this.login,
+            logout: this.logout,
+          }}
+        >
+          <MainNavigation />
+          <main className="main-content">
+            <Routes>
+              {!this.state.token && (
+                <Route
+                  path="/bookings"
+                  element={<Navigate replace to="/auth"></Navigate>}
+                  exact
+                />
+              )}
+
+              {this.state.token && (
+                <Route
+                  path="/"
+                  element={<Navigate replace to="/events"></Navigate>}
+                  exact
+                />
+              )}
+
+              {this.state.token && (
+                <Route
+                  path="/auth"
+                  element={<Navigate replace to="/events"></Navigate>}
+                  exact
+                />
+              )}
+              {!this.state.token && (
+                <Route
+                  path="/"
+                  element={<Navigate replace to="/auth"></Navigate>}
+                  exact
+                />
+              )}
+              {!this.state.token && (
+                <Route path="/auth" element={<AuthPage />} />
+              )}
+              <Route path="/events" element={<EventsPage />} />
+              {this.state.token && (
+                <Route path="/bookings" element={<BookingsPage />} />
+              )}
+            </Routes>
+          </main>
+        </AuthContext.Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
