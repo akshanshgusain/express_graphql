76791a1cc2a9b3eb0d23733169e469c3645da3e0
akshanshgusain <akshanshgusain@gmail.com>
2022-03-04 13:25:38 +0530

    feat: added navigation to ract app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,25 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
+import MainNavigation from "./components/navigation/MainNavigation";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate replace to="/auth"></Navigate>}
-          exact
-        />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-      </Routes>
+      <MainNavigation />
+      <main className="main-content">
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate replace to="/auth"></Navigate>}
+            exact
+          />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
